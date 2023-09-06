8ea7467e3705d16e1e2e24ad3a0aa400580d9e06
poliyka <jkny3586@gmail.com>
2023-09-07 00:33:28 +0800

    format code

--- config/launch.lua
+++ config/launch.lua
@@ -1,28 +1,28 @@
 local platform = require('utils.platform')()
 
 local options = {
-   default_prog = {},
-   launch_menu = {},
+  default_prog = {},
+  launch_menu = {},
 }
 
 if platform.is_win then
-   options.default_prog = { 'powershell' }
-   options.launch_menu = {
-      { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Ubuntu 20.04', args = { 'wsl' } },
-      {
-         label = 'Git Bash',
-         args = { 'C:\\Users\\poliyka\\scoop\\apps\\git\\current\\bin\\bash.exe' },
-      },
-   }
+  options.default_prog = { 'wsl' }
+  options.launch_menu = {
+    { label = 'PowerShell Desktop', args = { 'powershell' } },
+    { label = 'Command Prompt',     args = { 'cmd' } },
+    { label = 'Ubuntu 20.04',       args = { 'wsl' } },
+    {
+      label = 'Git Bash',
+      args = { 'C:\\Users\\poliyka\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+    },
+  }
 elseif platform.is_mac then
-   options.default_prog = { '/opt/homebrew/bin/zsh' }
-   options.launch_menu = {
-      { label = 'Bash', args = { 'bash' } },
-      { label = 'Nushell', args = { '/opt/homebrew/bin/nu' } },
-      { label = 'Zsh', args = { 'zsh' } },
-   }
+  options.default_prog = { '/opt/homebrew/bin/zsh' }
+  options.launch_menu = {
+    { label = 'Bash',    args = { 'bash' } },
+    { label = 'Nushell', args = { '/opt/homebrew/bin/nu' } },
+    { label = 'Zsh',     args = { 'zsh' } },
+  }
 end
 
 return options
